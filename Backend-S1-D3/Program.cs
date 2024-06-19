1c588615e7a38280141cce78c759bc6e69acf0bc
Frattins <davide.frattini1@gmail.com>
2024-06-19 16:16:43 +0200

    Aggiungere i file di progetto.

---
+++ Backend-S1-D3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend_S1_D3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
